3c74fa2047f418c42724247352258f8dcc082a2f
Eshan Shettennavar <shettennavareshan@gmail.com>
2026-01-30 16:38:09 +0530

    Fix prisma for postgres

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,7 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
 import 'dotenv/config'
 
-const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL ?? 'file:./dev.db' })
-const prisma = new PrismaClient({ adapter })
+const prisma = new PrismaClient()
 
 async function main() {
     const sources = [
